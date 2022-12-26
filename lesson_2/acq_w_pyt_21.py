b5ce153bc75b9259db69871cab35291856d4b03e
User <organik07@mail.ru>
2022-12-26 22:56:48 +0300

    lesson_2 modified task 1 and 3

--- lesson_2/acq_w_pyt_21.py
+++ lesson_2/acq_w_pyt_21.py
@@ -2,11 +2,11 @@ import re
 
 print("введите число")
 num = input()
-itnum = re.fullmatch(r'([1-9][0-9]*,[0-9]+|[1-9][0-9]*|0,[0-9]+)', num)
+itnum = re.fullmatch(r'-?([1-9][0-9]*,[0-9]+|[1-9][0-9]*|0,[0-9]+)', num)
 if itnum:
     summ = 0;
     for i in num:
-        if i == ",":
+        if i == "," or i == "-":
             continue
         else: summ = summ + int(i)
     print(summ)
--- lesson_2/acq_w_pyt_23.py
+++ lesson_2/acq_w_pyt_23.py
@@ -1,6 +1,6 @@
 print("введите натуральное число больше нуля")
 num = input()
 if num.isdecimal():
-    dict = {i: (1 + 1 / i) ** i for i in range(1, int(num)+1)}
+    dict = {i:  round(((1 + 1 / i) ** i), 2) for i in range(1, int(num)+1)}
     print(dict)
 else: print("вы ввели символ не соответствующий запросу")
